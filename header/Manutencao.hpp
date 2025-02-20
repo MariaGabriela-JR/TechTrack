04325fb71e391b346c29651bbede8183476af20e
Numseinemmeunome <alannakamura123@gmail.com>
2025-02-20 08:03:25 -0300

    manutencao

--- header/Manutencao.hpp
+++ header/Manutencao.hpp
@@ -1,4 +1,4 @@
-#ifndef MANUTENCAO_HPP
+/*#ifndef MANUTENCAO_HPP
 #define MANUTENCAO_HPP
 
 #include <string>
@@ -31,3 +31,66 @@ class Manutencao{
 };
 
 #endif
+*/
+#ifndef MANUTENCAO_HPP
+#define MANUTENCAO_HPP
+
+#include "Equipamento.hpp"
+#include "Cliente.hpp"
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Manutencao{
+
+    //Equipamento *equipamento;
+    //Cliente *cliente;
+
+private:
+
+
+
+    int id;
+    string Data;
+    string RM; 
+    string Categoria;
+    string Localizacao;
+    std::vector<string *> pecas;
+
+    string status;
+
+
+public:
+    Manutencao(const int& _id, const string& _data, const string& _rm, const string& _categoria, const string& _localizacao, /*std::vector<string *> _pecas,*/ const string& _status);
+    
+    int getId() const;
+    string getData() const;
+    string getRm() const;
+    string getCategoria() const;
+    string getLocalizacao() const;
+    string getpeças() const;
+
+
+    //getters e setters
+    //void setEquipamento(Equipamento*);
+    //void setCliente(Cliente*);
+
+    //Equipamento* getEquipamento();
+    //Cliente* getCliente();
+
+
+    void registrarManutencao(int rm, string data, string descricaoServiços, string *peças, string categoria, string status, string localizacao);
+
+    static void listarManutencao(const vector<Manutencao>& manutencao);
+
+    static vector<Manutencao> filtrarManutencao(const vector<Manutencao>& manutencao, const string& criterio);
+
+    static Manutencao* selecionarManutencao(vector<Manutencao>& manutencao, int idBusca);
+
+    void alterarStatusManutencao(Manutencao& manutencao, const string& novoStatus, const string& novaLocalizacao);
+
+};
+
+#endif 
--- src/Manutencao.cpp
+++ src/Manutencao.cpp
@@ -1,4 +1,4 @@
-# include <iostream>
+/*# include <iostream>
 # include "Manutencao.hpp"
 
 Manutencao::Manutencao(const string& _rm, const string& _status, const string& _descricao_da_manutencao, 
@@ -15,4 +15,91 @@ Manutencao::Manutencao(const string& _rm, const string& _status, const string& _
     string Manutencao :: getProblemasAnteriores() const {return problemas_anteriores;}
     string Manutencao :: getObservacoes() const {return observacoes;}
     string Manutencao :: getAtividades() const {return atividades;}
+
+*/
+    
+#include "Manutencao.hpp"
+#include <iostream>
+
+Manutencao::Manutencao(const int& _id, const string& _data, const string& _rm, const string& _categoria, const string& _localizacao, /*vector<string *> _pecas,*/ const string& _status)
+    : id(_id), Data(_data), RM(_rm), Categoria(_categoria), Localizacao(_localizacao), /*pecas(_pecas),*/ status(_status) {}
+
+int Manutencao::getId() const { return id; }
+string Manutencao::getData() const { return Data; }
+string Manutencao::getRm() const { return RM; }
+string Manutencao::getCategoria() const { return Categoria; }
+string Manutencao::getLocalizacao() const { return Localizacao; }
+
+
+
+string Manutencao::getpeças() const {//to considerando que vão ter varias peças para uma mesma manutenção
+    string result;
+    for (auto& peca : pecas) {
+        result += *peca + ", ";
+    }
+    return result.substr(0, result.size() - 2);
+}
+
+//getters e setters
+
+    //void setEquipamento(Equipamento* equipamento) { this->equipamento = equipamento; }
+   // void setCliente(Cliente* cliente) { this->cliente = cliente; }
+
+    //Equipamento* getEquipamento() { return this->equipamento; }
+    //Cliente* getCliente() { return this->cliente; }
+
+void Manutencao::registrarManutencao(int rm, string data, string descricaoServicos, string *peca, string categoria, string status, string localizacao) {
+    
+    RM = to_string(rm);
+    Data = data;
+    descricaoServicos = descricaoServicos;
+    pecas.push_back(peca);
+    status = status;
+    localizacao = localizacao;
+    
+    cout << "Manutencao registrada com sucesso!" << endl;
+
+}
+
+void Manutencao::listarManutencao(const vector<Manutencao>& manutencao) {
+    for (const auto& m : manutencao) {
+        cout << "ID: " << m.getId() << endl;
+        cout << "Data: " << m.getData() << endl;
+        cout << "RM: " << m.getRm() << endl;
+        cout << "Categoria: " << m.getCategoria() << endl;
+        cout << "Localizacao: " << m.getLocalizacao() << endl;
+        cout << "Pecas: " << m.getpeças() << endl;
+        cout << "Status: " << m.status << endl;
+        cout << "-----------------------------" << endl;
+    }
+}
+
+vector<Manutencao> Manutencao::filtrarManutencao(const vector<Manutencao>& manutencoes, const string& criterio) {
+    vector<Manutencao> filtered;
+    for (const auto& manutencao : manutencoes) {
+        if (manutencao.getRm().find(criterio) != string::npos) { //falta implementacao de cliente na manutencao
+            filtered.push_back(manutencao);
+        }
+    }
+    return filtered;
+}
+
+
+Manutencao* Manutencao::selecionarManutencao(vector<Manutencao>& manutencao, int idBusca) {
+    auto it = find_if(manutencao.begin(), manutencao.end(), [&idBusca](const Manutencao& manutencao) {
+        return manutencao.getId() == idBusca;
+    });
+    if (it != manutencao.end()) {
+        return &(*it);
+    }
+
+    return nullptr;
+}
+
+void Manutencao::alterarStatusManutencao(Manutencao& manutencao, const string& novoStatus, const string& novaLocalizacao) {
+    manutencao.status = novoStatus;
+    manutencao.Localizacao = novaLocalizacao;
+}
+
+
 
