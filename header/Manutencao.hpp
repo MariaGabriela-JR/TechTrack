36ad62ca8a397bde764e11150f99e47c32002871
Maria Gabriela J. Ramos <mg_jramos@hotmail.com>
2025-02-20 09:10:10 -0300

    Alteração em Manutenção.hpp e Manutenção.cpp

--- header/Manutencao.hpp
+++ header/Manutencao.hpp
@@ -1,95 +1,40 @@
-/*#ifndef MANUTENCAO_HPP
+#ifndef MANUTENCAO_HPP
 #define MANUTENCAO_HPP
 
 #include <string>
+#include <vector>
 using namespace std;
 
 class Manutencao{
 
     private:
-        string rm;
+        string rm; 
+        string data;
+        string descricao_de_servico;
+        string pecas;
+        string categoria;
         string status;
-        string descricao_da_manutencao;
-        string tipo_de_servico; 
-        string data_da_manutencao;
-        string problemas_anteriores; 
-        string observacoes; 
-        string atividades;
+        string localizacao;
+
 
     public:
-        Manutencao(const string& _rm, const string& _status, const string& _descricao_da_manutencao, const string& _tipo_de_servico, const string& _data_da_manutencao, const string& _problemas_anteriores, const string& _observacoes, const string& _atividade);
-
-        string getRm() const;
-        string getStatus() const;
-        string getDescricaoDaManutencao() const;
-        string getTipoDeServico() const;
-        string getDataDaManutencao() const;
-        string getProblemasAnteriores() const;
-        string getObservacoes() const;
-        string getAtividades() const;
-
-};
-
-#endif
-*/
-#ifndef MANUTENCAO_HPP
-#define MANUTENCAO_HPP
-
-#include "Equipamento.hpp"
-#include "Cliente.hpp"
-
-#include <string>
-#include <vector>
-
-using namespace std;
-
-class Manutencao{
-
-    //Equipamento *equipamento;
-    //Cliente *cliente;
-
-private:
-
-
-
-    int id;
-    string Data;
-    string RM; 
-    string Categoria;
-    string Localizacao;
-    std::vector<string *> pecas;
-
-    string status;
-
-
-public:
-    Manutencao(const int& _id, const string& _data, const string& _rm, const string& _categoria, const string& _localizacao, /*std::vector<string *> _pecas,*/ const string& _status);
+        Manutencao(const string& _rm, const string& _data, const string& _descricao_de_servico, const string& _pecas, const string& _categoria, const string& _status, const string& _localizacao);
+        
     
-    int getId() const;
-    string getData() const;
-    string getRm() const;
-    string getCategoria() const;
-    string getLocalizacao() const;
-    string getpeças() const;
+        string getRm() const;
+        string getData() const;
+        string getDescricaoDeServico() const;
+        string getPecas() const;
+        string getCategoria() const;
+        string getStatus() const;
+        string getLocalizacao() const;
 
 
-    //getters e setters
-    //void setEquipamento(Equipamento*);
-    //void setCliente(Cliente*);
-
-    //Equipamento* getEquipamento();
-    //Cliente* getCliente();
-
-
-    void registrarManutencao(int rm, string data, string descricaoServiços, string *peças, string categoria, string status, string localizacao);
-
-    static void listarManutencao(const vector<Manutencao>& manutencao);
-
-    static vector<Manutencao> filtrarManutencao(const vector<Manutencao>& manutencao, const string& criterio);
-
-    static Manutencao* selecionarManutencao(vector<Manutencao>& manutencao, int idBusca);
-
-    void alterarStatusManutencao(Manutencao& manutencao, const string& novoStatus, const string& novaLocalizacao);
+        void registrarManutencao(int rm, string data, string descricaoServiços, string *peças, string categoria, string status, string localizacao);
+        static void listarManutencao(const vector<Manutencao>& manutencao);
+        static vector<Manutencao> filtrarManutencao(const vector<Manutencao>& manutencao, const string& criterio);
+        static Manutencao* selecionarManutencao(vector<Manutencao>& manutencao, int idBusca);
+        void alterarStatusManutencao(Manutencao& manutencao, const string& novoStatus, const string& novaLocalizacao);
 
 };
 
